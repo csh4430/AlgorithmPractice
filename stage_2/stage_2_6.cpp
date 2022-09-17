4b822e827ee41572f3bdc5509a365cb06165b70d
csh4430 <snghun6889@gmail.com>
2022-09-18 00:36:16 +0900

    a

---
+++ stage_2/stage_2_6.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(void)
+//{
+//	unsigned int a, b;
+//	unsigned int c;
+//
+//	int h, m;
+//
+//	cin >> a >> b;
+//	cin >> c;
+//
+//	if (a > 24 || b > 60 || c > 1001) return 0;
+//
+//	m = b + c;
+//	h = m / 60;
+//	m = m % 60;
+//
+//	h += a;
+//	
+//	h %= 24;
+//
+//	cout << h << ' ' << m;
+//
+//	return 0;
+//}
