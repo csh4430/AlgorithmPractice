a8440516d292bd0a7c98f2026348448ce2a808d2
csh4430 <snghun6889@gmail.com>
2022-09-18 02:03:51 +0900

    Create stage_3_2.cpp

---
+++ stage_3/stage_3_2.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(void)
+//{
+//	int n;
+//	int a, b;
+//	cin >> n;
+//	int* temp = new int[n];
+//	for (int i = 0; i < n; i++) {
+//		cin >> a >> b;
+//		temp[i] = a + b;
+//	}
+//	for (int i = 0; i < n; i++) {
+//		cout << temp[i] << '\n';
+//	}
+//	return 0;
+//}
