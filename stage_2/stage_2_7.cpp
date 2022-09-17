85e3b538460ae71f52ce6eb2118b9a6351d792c2
csh4430 <snghun6889@gmail.com>
2022-09-18 01:53:16 +0900

    Update stage_2_7.cpp

--- stage_2/stage_2_7.cpp
+++ stage_2/stage_2_7.cpp
@@ -1,57 +1,35 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int max(int a, int b, int c, int* ptr = nullptr) {
-//	int temp;
-//	if (a > b) {
-//		temp = a;
-//		if(ptr != nullptr)
-//			*ptr = 0;
-//	}
-//	else {
-//		temp = b;
-//		if (ptr != nullptr)
-//			*ptr = 1;
-//	}
-//	if (temp < c) {
-//		temp = c;
-//		if (ptr != nullptr)
-//			*ptr = 2;
-//	}
-//	return temp;
-//}
-//
-//int main(void)
-//{
-//	int dice[3];
-//	int temp[3] = {0,};
-//	int idx;
-//	int cnt;
-//	int result = 0;
-//	cin >> dice[0] >> dice[1] >> dice[2];
-//
-//	for (int i = 0; i < 3; i++) {
-//		for (int j = 0; j < 3; j++) {
-//			if (dice[i] == dice[j])
-//				temp[i]++;
-//		}
-//	}
-//
-//	cnt = max(temp[0], temp[1], temp[2], &idx);
-//
-//	switch (cnt) {
-//	case 3:
-//		result = 10000 + dice[idx] * 1000;
-//		break;
-//	case 2:
-//		result = 1000 + dice[idx] * 100;
-//		break;
-//	case 1:
-//		result = max(dice[0], dice[1], dice[2]) * 100;
-//	}
-//
-//	cout << result;
-//
-//	return 0;
-//}
+#include <iostream>
+
+using namespace std;
+
+int max(int a, int b, int c) {
+	int temp;
+	if (a > b) {
+		temp = a;
+	}
+	else {
+		temp = b;
+	}
+	if (temp < c) {
+		temp = c;
+	}
+	return temp;
+}
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a == b && b == c) {
+		cout << 10000 + a * 1000;
+	}
+	else if (a == b || a == c) {
+		cout << 1000 + a * 100;
+	}
+	else if (b == c) {
+		cout << 1000 + b * 100;
+	}
+	else {
+		cout << max(a, b, c) * 100;
+	}
+	return 0;
+}
