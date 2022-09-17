647cc6044b26912f8285e0e792a807943ef57dd1
csh4430 <snghun6889@gmail.com>
2022-09-17 10:22:21 +0900

    ars

---
+++ stage_1/stage_1_10.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(void)
+//{
+//	int ki, qu, lo, bi, kn, pa;
+//	cin >> ki >> qu >> lo >> bi >> kn >> pa;
+//
+//	cout << 1 - ki << " ";
+//	cout << 1 - qu << " ";
+//	cout << 2 - lo << " ";
+//	cout << 2 - bi << " ";
+//	cout << 2 - kn << " ";
+//	cout << 8 - pa;
+//	return 0;
+//}
---
+++ stage_1/stage_1_9.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(void)
+//{
+//	int offset = 2541 - 1998;
+//	int thai_year = 0;
+//	cin >> thai_year;
+//
+//	cout << thai_year - offset;
+//
+//	return 0;
+//}
