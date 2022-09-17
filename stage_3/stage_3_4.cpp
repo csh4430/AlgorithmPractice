5a81227a6c8fd9754a4a2ca1e8051df8968ed89a
csh4430 <snghun6889@gmail.com>
2022-09-18 03:18:12 +0900

    Create stage_3_4.cpp

---
+++ stage_3/stage_3_4.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(void)
+//{
+//	int t;
+//	int a, b;
+//	cin >> t;
+//	int* temp = new int[t];
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cout.tie(NULL);
+//	for (int i = 0; i < t; i++) {
+//		cin >> a >> b;
+//		temp[i] = a + b;
+//	}
+//
+//	for (int i = 0; i < t; i++) {
+//		cout << temp[i] << '\n';
+//	}
+//}
