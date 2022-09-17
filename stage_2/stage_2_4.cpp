550733d944271f8ebd7dbbdaacbcc3d7cacebb9b
csh4430 <snghun6889@gmail.com>
2022-09-17 11:51:55 +0900

    Create stage_2_4.cpp

---
+++ stage_2/stage_2_4.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(void)
+//{
+//	int x, y;
+//	cin >> x >> y;
+//	int result = 0;
+//	if ((x < -1000 || x > 1000) && ((y < -1000 || y > 1000))) return 0;
+//
+//	if (x * y > 0) {
+//		if (x > 0)
+//			result = 1;
+//		else
+//			result = 3;
+//	}
+//	else {
+//		if (x > 0)
+//			result = 4;
+//		else
+//			result = 2;
+//	}
+//	cout << result;
+//	return 0;
+//}
