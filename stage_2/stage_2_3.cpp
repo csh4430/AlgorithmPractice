4db3ec37ed8af018788a4cea6f507ae274832b55
csh4430 <snghun6889@gmail.com>
2022-09-17 11:15:13 +0900

    Create stage_2_3.cpp

---
+++ stage_2/stage_2_3.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(void)
+//{
+//	unsigned int year;
+//	int temp = 0;
+//
+//	cin >> year;
+//
+//	if (!year) return 0;
+//	if (year > 4000) return 0;
+//
+//	if (year % 400 == 0) {
+//		temp = 1;
+//	}
+//	if (year % 4 == 0 && year % 100 != 0) {
+//		temp = 1;
+//	}
+//
+//	cout << temp;
+//
+//	return 0;
+//}
