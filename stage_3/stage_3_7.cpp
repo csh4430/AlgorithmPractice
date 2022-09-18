c03e80a59612fcabcbbc988dde66249d12129547
csh4430 <snghun6889@gmail.com>
2022-09-18 15:06:07 +0900

    a

---
+++ stage_3/stage_3_7.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(void)
+//{
+//	int* a, *b;
+//	int t;
+//	cin >> t;
+//	a = new int[t];
+//	b = new int[t];
+//
+//	for (int i = 0; i < t; i++) {
+//		cin >> a[i] >> b[i];
+//	}
+//
+//	for (int i = 0; i < t; i++) {
+//		cout << "Case #" << i + 1 << ": ";
+//		cout << a[i] << " + " << b[i] << " = ";
+//		cout << a[i] + b[i] << '\n';
+//	}
+//	return 0;
+//}
