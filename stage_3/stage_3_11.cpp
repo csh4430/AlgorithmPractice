978e97edb02ab8e56085ec1984d8c31b0366ed1a
csh4430 <snghun6889@gmail.com>
2022-09-18 17:53:20 +0900

    Create stage_3_11.cpp

---
+++ stage_3/stage_3_11.cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(void)
+//{
+//	int a = 0, b = 0;
+//	int cnt = 0;
+//	int* temp = new int[100];
+//
+//	while(1) {
+//		cin >> a >> b;
+//		if (a == 0 && b == 0)
+//		{
+//			break;
+//		}
+//		temp[cnt] = a + b;
+//		cnt++;
+//	}
+//	for (int i = 0; i < cnt; i++) {
+//		cout << temp[i] << '\n';
+//	}
+//	return 0;
+//}
