7d274daf3c874c0a5fa2eeb02b54d66a43f94702
csh4430 <snghun6889@gmail.com>
2022-09-18 17:46:00 +0900

    Create stage_3_10.cpp

---
+++ stage_3/stage_3_10.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(void)
+//{
+//	int n, x;
+//	int a;
+//	cin >> n >> x;
+//
+//	for (int i = 0; i < n; i++) {
+//		cin >> a;
+//		if (a < x)
+//			cout << a << ' ';
+//	}
+//	return 0;
+//}
