b224f44c4a6acc80e8374405acf62d5f2e35b90d
csh4430 <snghun6889@gmail.com>
2022-09-18 18:05:14 +0900

    Create stage_3_12.cpp

---
+++ stage_3/stage_3_12.cpp
@@ -0,0 +1,16 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(void)
+//{
+//	int a, b;
+//	int cnt = 0;
+//	while (!(cin >> a >> b).eof()) {
+//		if (a <= 0 || b >= 10)
+//			return 0;
+//		cout << a + b << '\n';
+//
+//	}
+//	return 0;
+//}
