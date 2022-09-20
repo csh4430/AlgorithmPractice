d4d9e2aab1ed5aa85fa2912d70e7b376e09469e9
csh4430 <snghun6889@gmail.com>
2022-09-20 15:23:34 +0900

    Create stage_4_2.cpp

---
+++ stage_4/stage_4_2.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main(void)
+//{
+//	int arr[9];
+//	int max = 0, id;
+//
+//	for (int i = 0; i < 9; i++) {
+//		cin >> arr[i];
+//		if (arr[i] > max) {
+//			max = arr[i];
+//			id = i + 1;
+//		}
+//	}
+//	cout << max << '\n' << id;
+//
+//	return 0;
+//}
