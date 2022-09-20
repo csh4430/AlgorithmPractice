293d72cbdac1da6bba2ad143799adc88eac8a008
csh4430 <snghun6889@gmail.com>
2022-09-20 15:42:07 +0900

    Create stage_4_3.cpp

---
+++ stage_4/stage_4_3.cpp
@@ -0,0 +1,20 @@
+	//#include <iostream>
+	//using namespace std;
+
+	//int main(void)
+	//{
+	//	int arr[42] = {0, };
+	//	int t;
+	//	int cnt = 0;
+
+	//	for (int i = 0; i < 10; i++) {
+	//		cin >> t;
+	//		arr[t % 42] ++;
+	//	}
+	//	for (int i = 0; i < 42; i++) {
+	//		if (arr[i] != 0)
+	//			cnt++;
+	//	}
+	//	cout << cnt;
+	//	return 0;
+	//}
