194432496e1f05fa53fbc7696e3ad83a7920e68a
csh4430 <snghun6889@gmail.com>
2022-09-20 15:03:30 +0900

    Update stage_3_13.cpp

--- stage_3/stage_3_13.cpp
+++ stage_3/stage_3_13.cpp
@@ -1,23 +1,24 @@
-#include <iostream>
-
-using namespace std;
-
-int main(void)
-{
-	int n;
-	int m = 0;
-	int cnt = 0;
-	int temp;
-	cin >> n;
-	if (n < 0 || n > 99)
-		return 0;
-	int a = n;
-	while (m != n) {
-		temp = a / 10 + a % 10;
-		m = (a % 10) * 10 + temp % 10;
-		cnt++;
-		a = m;
-	}
-	cout << cnt;
-	return 0;
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(void)
+//{
+//	int n;
+//	int m = 0;
+//	int cnt = 0;
+//	int temp;
+//	cin >> n;
+//	if (n < 0 || n > 99)
+//		return 0;
+//	int a = n;
+//	do {
+//		temp = a / 10 + (a % 10);
+//		m = (a % 10) * 10 + (temp % 10);
+//		cnt++;
+//		a = m;
+//	} while (m != n);
+//	
+//	cout << cnt;
+//	return 0;
+//}
