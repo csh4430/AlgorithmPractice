56621527307dcd2e3397dab4e2a4f2a6e1d084c5
csh4430 <snghun6889@gmail.com>
2022-10-15 17:22:54 +0900

    s

--- stage_7/stage_7_1.cpp
+++ stage_7/stage_7_1.cpp
@@ -1,22 +1,22 @@
-#include <iostream>
-
-using namespace std;
-
-int main(void)
-{
-	long long int A, B, C;
-	cin >> A >> B >> C;
-	if (B >= C)
-	{
-		cout << -1;
-		return 0;
-	}
-
-	
-	int cnt = - A / (B - C) + 1;
-
-	cout << cnt;
-	
-
-	return 0;
-}
+// #include <iostream>
+//
+// using namespace std;
+//
+// int main(void)
+// {
+// 	long long int A, B, C;
+// 	cin >> A >> B >> C;
+// 	if (B >= C)
+// 	{
+// 		cout << -1;
+// 		return 0;
+// 	}
+//
+// 	
+// 	int cnt = - A / (B - C) + 1;
+//
+// 	cout << cnt;
+// 	
+//
+// 	return 0;
+// }
