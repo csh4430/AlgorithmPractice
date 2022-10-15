b8d24c5f55ab7551101b8297a1faf997d4716d99
csh4430 <snghun6889@gmail.com>
2022-10-15 17:33:10 +0900

    s

---
+++ stage_26/stage_26_2.cpp
@@ -0,0 +1,46 @@
+﻿#include <complex.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+vector<int> arr[100001];
+int visited[100001] = {0,};
+int rh[100001] = {0,};
+int cnt = 1;
+void dfs(int r)
+{
+	visited[r] = true;
+	rh[r] = cnt;
+	cnt++;
+	for(int x : arr[r])
+	{
+		if(!visited[x])
+		{
+			dfs(x);
+		}
+	}
+}
+int main(void)
+{
+	int n, m, r;
+	int nT;
+	int mT;
+	cin >> n >> m >> r;
+	for(int i = 0; i < m; i ++)
+	{
+		cin >> nT >> mT;
+		arr[nT].push_back(mT);	
+		arr[mT].push_back(nT);	
+	}
+	for (int i = 0; i <= n; i++) {
+		sort(arr[i].begin(), arr[i].end(), std::greater<>());
+		
+	}
+	dfs(r);
+	for(int i = 1; i <= n; i++)
+	{
+		cout << rh[i] << '\n';
+	}
+	return 0;
+}
